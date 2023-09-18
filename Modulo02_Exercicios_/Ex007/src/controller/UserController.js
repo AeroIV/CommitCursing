7b24e119ab2a5ac6e3058b4e0b693b8828826c99
lucas <die.nasc.contact@gmail.com>
2023-09-18 11:08:12 -0300

    #

--- Modulo02_Exercicios_/Ex007/src/controller/UserController.js
+++ Modulo02_Exercicios_/Ex007/src/controller/UserController.js
@@ -26,4 +26,50 @@ module.exports = {
             res.json({ error });
         }
     },
+
+    async searchUser(req, res) {
+        const { id } = req.params;
+
+        try {
+            const user = await prisma.user.findUnique({
+                where: { id: parseInt(id, 10) },
+            });
+            res.json(user);
+        } catch (error) {
+            res.json(error);
+        }
+    },
+
+    async searchUsers(req, res) {
+        const users = await prisma.user.findMany();
+        res.json(users);
+    },
+
+    async updateUser(req, res) {
+        try {
+            const { id } = req.params;
+            const { nome, email } = req.body;
+
+            const user = await prisma.user.findUnique({
+                where: { id: parseInt(id) },
+            });
+
+            if (!user) {
+                return res.json({ error: "ERROR: This user don't exist, please try again." });
+            }
+
+            user = await prisma.user.update({
+                where: { id: parseInt(id) },
+                data: { nome, email },
+                select: {
+                    nome: true,
+                    email: true
+                }
+            });
+            return res.json(user);
+
+        } catch (error) {
+            res.json(error)
+        }
+    },
 };
--- Modulo02_Exercicios_/Ex007/src/routes.js
+++ Modulo02_Exercicios_/Ex007/src/routes.js
@@ -3,6 +3,9 @@ const UserController = require('./controller/UserController');
 const router = Router();
 
 router.post('/criarUser', UserController.criarUsuario);
+router.get('/searchUser/:id', UserController.searchUser);
+router.get('/searchUser', UserController.searchUsers);
+router.put('/updateUser/:id', UserController.updateUser);
 
 module.exports = { router };
 
